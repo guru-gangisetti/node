ee7a9e0e5b28ddc0c73ef428acdd614e14db976f
Guru Gangisetti <gurugangisetti@IS-5241s-MacBook-Pro.local>
2021-03-16 14:15:40 -0700

    updated files

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv'
 
-const app = express();
-const PORT = process.env.PORT || 5000
+const app =  express();
+dotenv.config();
 
-app.get('/', (req, res)=>{
-    res.send('Hello');
+app.use(bodyParser.json({limit :  "30mb" , extended: true}));
+app.use(bodyParser.urlencoded({limit :  "30mb" , extended: true}));
+app.use(cors());
+
+//app.use( '/posts', postRoutes);
+
+app.get( '/', (req, res)=>{
+    res.send('Hellow Memories API');
 });
 
-app.listen(PORT, ()=>{
-    console.log('server listening at', PORT);
-})
+const CONNECTION_URL = "mongodb+srv://gurubrahmam:Sairam12@cluster0.zkm75.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000; 
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser : true, useUnifiedTopology : true})
+            .then(() => app.listen(PORT, ()=> console.log(`Server running on : ${PORT} `) ))
+            .catch(() => console.log(error));
+
+mongoose.set('useFindAndModify', false);
+
+
